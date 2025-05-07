8a84618e4f6ef2a3c8569cdcb8f601b45c428c86
Theophilus Kolawole <theophiluskolawole19@gmail.com>
2025-05-07 14:28:57 +0100

    portfolio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Portfolio from './Pages/Portfolio';
+import Portfolio from './pages/Portfolio';
 import { ThemeProvider } from './context/ThemeContext';
 
 export default function App() {
